bb0b2ee6e77f3c4ad5c8a01122de923b6521a132
Jothaku <jothagf@gmail.com>
2023-05-12 16:18:24 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,175 @@
+import React, { useState } from "react";
 import "./App.css";
 import { registerUser } from "./services/registerUser";
 
-export function App() {
-  const onSubmit = (data) => {
-    registerUser(data);
+function App() {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [password, setPassword] = useState("");
+  const [verifyPassword, setVerifyPassword] = useState("");
+  const [formValidation, setFormValidation] = useState({
+    email: undefined,
+    name: undefined,
+    age: undefined,
+    password: undefined,
+    verifyPassword: undefined,
+  });
+
+  const isformValid = Object.keys(formValidation).every(
+    (key) => formValidation[key] === ""
+  );
+  const [termsAccepted, setTermsAccepted] = useState(false);
+
+  const handleSumbit = (event) => {
+    event.preventDefault();
+    if (isformValid) {
+      registerUser(email, name, age, password);
+    }
+  };
+
+  const handleEmailChange = (event) => {
+    const value = event.target.value;
+    setFormValidation({
+      ...formValidation,
+      email: value.length == 0 ? "email is required" : "",
+    });
+
+    setEmail(value);
+  };
+  const handleNameChange = (event) => {
+    const value = event.target.value;
+    setFormValidation({
+      ...formValidation,
+      name: value.length == 0 ? "name is required" : "",
+    });
+
+    setName(value);
+  };
+
+  const handleageChange = (event) => {
+    const value = event.target.value;
+    let errorMessage = "";
+
+    if (value.length === 0) {
+      errorMessage = "Age is required";
+    } else if (value < 18) {
+      errorMessage = "You must be above 18 to register";
+    }
+
+    setAge(value);
+    setFormValidation({
+      ...formValidation,
+      age: errorMessage,
+    });
+  };
+
+  const handlepasswordChange = (event) => {
+    const value = event.target.value;
+
+    if (value.length === 0) {
+      setFormValidation({
+        ...formValidation,
+        password: "password is required",
+      });
+    } else if (value.length < 5) {
+      setFormValidation({
+        ...formValidation,
+        password: "password is too short",
+      });
+    } else {
+      setFormValidation({
+        ...formValidation,
+        password: "",
+      });
+    }
+
+    setPassword(value);
+  };
+
+  const handleverifyPasswordChange = (event) => {
+    const value = event.target.value;
+    setFormValidation({
+      ...formValidation,
+      verifyPassword: value !== password ? "passwords do not match" : "",
+    });
+
+    setVerifyPassword(value);
   };
 
   return (
     <div>
-      <form>
+      <form onSubmit={handleSumbit}>
         <div>
-          <label>
-            Email
-            <input type="email" placeholder="Email" />
-          </label>
-          <span className="error" role="alert">
-            Test error
-          </span>
+          <label> Email </label>
+          <input
+            value={email}
+            placeholder="Email"
+            type="Email"
+            onChange={handleEmailChange}
+          />
+          {formValidation.email && (
+            <span style={{ color: "red" }}>{formValidation.email}</span>
+          )}
         </div>
         <div>
-          <label>
-            Name
-            <input type="text" placeholder="Name" />
-          </label>
-          <span className="error" role="alert"></span>
+          <label>Name</label>
+          <input
+            value={name}
+            placeholder="Name"
+            type="Name"
+            onChange={handleNameChange}
+          />
+          {formValidation.name && (
+            <span style={{ color: "red" }}>{formValidation.name}</span>
+          )}
         </div>
         <div>
-          <label>
-            Age
-            <input type="number" placeholder="Age" />
-          </label>
-          <span className="error" role="alert"></span>
+          <label>Age</label>
+          <input
+            value={age}
+            placeholder="Age"
+            type="number"
+            onChange={handleageChange}
+            step="1"
+          />
+          {formValidation.age && (
+            <span style={{ color: "red" }}>{formValidation.age}</span>
+          )}
         </div>
         <div>
-          <label>
-            Password
-            <input type="password" placeholder="Password" />
-          </label>
-          <span className="error" role="alert"></span>
+          <label>Password</label>
+          <input
+            value={password}
+            placeholder="Password"
+            type="Password"
+            onChange={handlepasswordChange}
+          />
+          {formValidation.password && (
+            <span style={{ color: "red" }}>{formValidation.password}</span>
+          )}
         </div>
         <div>
-          <label>
-            Password check
-            <input type="password" placeholder="Password check" />
-          </label>
-          <span className="error" role="alert"></span>
+          <label>Verify Password</label>
+          <input
+            value={verifyPassword}
+            placeholder="Verify Password"
+            type="password"
+            onChange={handleverifyPasswordChange}
+          />
         </div>
-        <div>
-          <label>
-            <input type="checkbox" />
-            Accept terms & conditions: Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit. Pellentesque pharetra, tortor ac placerat
-            elementum, neque libero luctus mi, ut efficitur nisl mauris at nisl.
-            Suspendisse non neque et neque facilisis convallis. Praesent erat
-            magna, sollicitudin eu porttitor ut, tincidunt sit amet urna.
-            Vestibulum congue neque metus.
-          </label>
-          <span className="error" role="alert"></span>
-        </div>
-
-        <button>Sign up</button>
+        <label>
+          <input
+            type="checkbox"
+            checked={termsAccepted}
+            onChange={(event) => setTermsAccepted(event.target.checked)}
+          />
+          Please read and accept the terms and conditions
+        </label>
+        <button disabled={!isformValid}>Sig up</button>
       </form>
     </div>
   );
 }
+
+export default App;
--- test-config/utils.js
+++ test-config/utils.js
@@ -1,12 +1,7 @@
-/* eslint-disable import/export */
 import { cleanup, render } from "@testing-library/react";
 import { afterEach } from "vitest";
 
-afterEach(() => {
-  cleanup();
-});
-
-const customRender = (ui, options = {}) =>
+export const customRender = (ui, options = {}) =>
   render(ui, {
     // wrap provider(s) here if needed
     wrapper: ({ children }) => children,
@@ -15,5 +10,7 @@ const customRender = (ui, options = {}) =>
 
 export * from "@testing-library/react";
 export { default as userEvent } from "@testing-library/user-event";
-// override render export
-export { customRender as render };
+
+afterEach(() => {
+  cleanup();
+});
